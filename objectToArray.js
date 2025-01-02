2c99949762c7e9bb4fffd95bd55e4b96332bf858
HafsaBenabla <benablahafsa@gmail.com>
2025-01-02 21:31:35 +0100

    modification du code bjectToArray

--- objectToArray.js
+++ objectToArray.js
@@ -1,10 +1,15 @@
 function objectToArray(obj) {
-    let resultat = [];
-    for (let key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            resultat.push([key, obj[key]]);
-        }
-
+    let keys = Object.keys(obj);  // Récupère les clés de l'objet
+    let resultat = [];  
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];  
+        let value = obj[key];  
+        resultat.push([key, value]); 
     }
-    return resultat;
+
+    return resultat;  
 }
+
+
+let obj = { a: 1, b: 2, c: 3 };
+console.log(objectToArray(obj)); 
